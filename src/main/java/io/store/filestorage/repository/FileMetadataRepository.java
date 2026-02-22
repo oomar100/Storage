ea85f868bbee75820d1cc00b75829903f973100e
oomar400 <omaromer92@gmail.com>
2026-02-23 01:26:43 +0200

    Added userid queries to FileMetadata repo

--- src/main/java/io/store/filestorage/repository/FileMetadataRepository.java
+++ src/main/java/io/store/filestorage/repository/FileMetadataRepository.java
@@ -1,9 +1,14 @@
 package io.store.filestorage.repository;
 
 import io.store.filestorage.entity.FileMetadata;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FileMetadataRepository extends JpaRepository<FileMetadata, UUID> {
+    Page<FileMetadata> findByUserId(String userId, Pageable pageable);
+    Optional<FileMetadata> findByIdAndUserId(UUID id, String userId);
 }
