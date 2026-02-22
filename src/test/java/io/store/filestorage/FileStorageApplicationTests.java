1ce023535892eea03bca2058c2594f3a4bb51bb2
oomar400 <omaromer92@gmail.com>
2026-02-22 23:30:05 +0200

    Project init

---
+++ src/test/java/io/store/filestorage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.store.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
